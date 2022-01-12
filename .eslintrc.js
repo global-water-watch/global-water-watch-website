3072b5c13a1339e6497e4205aa9dc332bb783359
Vincent van Dijck <vincent@voorhoede.nl>
2022-01-12 11:39:07 +0100

    chore(eslint) adjust rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,10 +12,13 @@ module.exports = {
     '@nuxtjs',
     'plugin:nuxt/recommended',
   ],
-  plugins: [
-  ],
   // add your custom rules here
   rules: {
+    indent: 'off',
     'comma-dangle': ['warn', 'always-multiline'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    semi: process.env.NODE_ENV === 'production' ? ['error', 'never'] : ['warn', 'never'],
+    'vue/script-indent': ['error', 2, { baseIndent: 1 }],
   },
 }
