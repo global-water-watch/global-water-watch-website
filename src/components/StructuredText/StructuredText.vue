83b236689b7a30365c06b836ebcb63fdc6599163
Benedikt Deitmer <you@hornebom.com>
2022-01-12 14:52:57 +0100

    - adds `blue-light` color variable - splits up scss file for `MultiTextSection` - sets `title` as optional prop - adds styling for `StructuredText`

--- src/components/StructuredText/StructuredText.vue
+++ src/components/StructuredText/StructuredText.vue
@@ -1,11 +1,13 @@
 <template>
-  <DatocmsStructuredText :data="text" />
+  <DatocmsStructuredText :data="text" class="structured-text" />
 </template>
 
 <script>
-export default {
-  props: {
-    text: { type: Object, required: true },
-  },
-}
+  export default {
+    props: {
+      text: { type: Object, required: true },
+    },
+  }
 </script>
+
+<style src="./StructuredText.scss" lang="scss"></style>
