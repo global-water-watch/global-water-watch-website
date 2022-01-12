def3b0593cf3d97c4edad272108e084d9887f89a
Vincent van Dijck <vincent@voorhoede.nl>
2022-01-12 12:04:27 +0100

    feat(components) new AppLink component

---
+++ src/components/AppLink/AppLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <component
+    :is="to ? 'router-link' : 'a'"
+    :to="to ? to : null"
+    class="app-link"
+    v-bind="$attrs"
+  >
+    <slot />
+  </component>
+</template>
+
+<script>
+  export default {
+    props: {
+      to: {
+        type: [String, Object],
+        validator (value) {
+          if (!value || typeof value === 'string') {
+            return true
+          }
+
+          return (
+            value.name && (value.params)
+              ? Object.keys(value.params).length > 0
+              : true
+          )
+        },
+        default: '',
+      },
+    },
+  }
+</script>
+
+<style src="./AppLink.scss" lang="scss"></style>
