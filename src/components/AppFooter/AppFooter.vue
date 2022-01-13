766e4e3daf728f2aabbb8dffb0a5d07ef605e5f1
Vincent van Dijck <vincent@voorhoede.nl>
2022-01-13 10:19:47 +0100

    fix(app-footer) revert changes

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -1,34 +1,28 @@
 <template>
-  <footer role="contentinfo" class="app-footer layout-section layout-section--force-lined">
-    <v-container>
-      <v-row no-gutters>
-        <v-col>
-          <Wave class="app-footer__wave" />
-          <h2 class="h2">
-            {{ title }}
-          </h2>
-          <p>{{ subtitle }}</p>
-        </v-col>
-      </v-row>
-      <v-row no-gutters>
-        <v-col>
-          <div class="app-footer__bottom app-footer__flex-center">
-            <p class="small">
-              Copyright {{ year }} Deltares
-            </p>
-            <nav class="small">
-              <ul class="app-footer__legals app-footer__flex-center">
-                <li v-for="link in links" :key="link.id">
-                  <AppLink :link="link">
-                    {{ link.title }}
-                  </AppLink>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
+  <footer role="contentinfo" class="layout-section layout-section--force-lined app-footer">
+    <div class="layout-container app-footer__inner">
+      <Wave class="app-footer__wave" />
+      <h2 class="h2">
+        {{ title }}
+      </h2>
+      <p>{{ subtitle }}</p>
+
+      <div class="app-footer__bottom app-footer__flex-center">
+        <p class="small">
+          Copyright {{ year }} Deltares
+        </p>
+
+        <nav class="small">
+          <ul class="app-footer__legals app-footer__flex-center">
+            <li v-for="link in links" :key="link.id">
+              <AppLink :link="link">
+                {{ link.title }}
+              </AppLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </footer>
 </template>
 
