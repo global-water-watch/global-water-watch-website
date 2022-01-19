1050e1e3e481574c331e6dbc161145614c81cd1a
Benedikt Deitmer <you@hornebom.com>
2022-01-19 18:54:43 +0100

    - refactor to be in line with existing concept shown on Home page - dato models updated as well

--- src/pages/blog/_slug.vue
+++ src/pages/blog/_slug.vue
@@ -1,21 +1,11 @@
 <template>
-  <BlogHero
-    :title="page.title"
-    :summary="page.summary"
-    :image="page.image"
-    :author="page.author"
-  />
+  <PageHeroes :sections="page.heroes" />
 </template>
 
 <script>
   import query from './_slug.query.graphql'
-  import BlogHero from '@/components/PageHeroes/BlogHero/BlogHero'
 
   export default {
-    components: {
-      BlogHero,
-    },
-
     async asyncData ({ $datocms, $preview, params }) {
       const variables = { slug: params.slug }
       const { blog } = await $datocms.fetchData({ query, variables, preview: !!$preview })
