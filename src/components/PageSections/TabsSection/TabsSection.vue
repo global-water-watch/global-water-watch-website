b906b21a3e091d9b8090486261bc1a9f79b1324d
Vincent van Dijck <vincent@voorhoede.nl>
2022-01-19 11:59:02 +0100

    feat(components) new ResponsiveVideo component

--- src/components/PageSections/TabsSection/TabsSection.vue
+++ src/components/PageSections/TabsSection/TabsSection.vue
@@ -20,7 +20,8 @@
           <v-container fluid>
             <v-row no-gutters>
               <v-col class="col-12 col-md-6">
-                <ResponsiveImage :image="item.image" :contain="true" />
+                <ResponsiveImage v-if="Boolean(item.image)" :image="item.image" :contain="true" />
+                <ResponsiveVideo v-if="Boolean(item.video)" :video="item.video" />
               </v-col>
 
               <v-col class="col-12 col-md-6">
@@ -38,7 +39,14 @@
 </template>
 
 <script>
+  import ResponsiveImage from '@/components/ResponsiveImage/ResponsiveImage'
+  import ResponsiveVideo from '@/components/ResponsiveVideo/ResponsiveVideo'
+
   export default {
+    components: {
+      ResponsiveImage,
+      ResponsiveVideo,
+    },
     props: {
       id: {
         type: String,
---
+++ src/components/ResponsiveVideo/ResponsiveVideo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="responsive-video">
+    <vue-dato-video
+      :loop="true"
+      :autoplay="true"
+      :mute="true"
+      :video="video"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      video: {
+        type: Object,
+        required: true,
+      },
+    },
+  }
+</script>
