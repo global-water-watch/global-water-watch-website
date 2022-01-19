94f970081996684c571e64eedb998fe0241a6ba4
Benedikt Deitmer <you@hornebom.com>
2022-01-19 11:45:07 +0100

    - creates Avatar component - creates fragment for PeopleSection - styling and markup PeopleSection

--- src/components/PageSections/PeopleSection/PeopleSection.vue
+++ src/components/PageSections/PeopleSection/PeopleSection.vue
@@ -1,10 +1,22 @@
 <template>
   <section
     :id="id"
-    class="layout-section layout-section--lined layout-section--padded"
+    class="layout-section layout-section--lined layout-section--padded people-section"
   >
     <div class="layout-container">
-      <h2>@todo: people section</h2>
+      <h2 class="h2 ">
+        {{ title }}
+      </h2>
+      <ul class="people-section__list">
+        <li v-for="person in persons" :key="person.id">
+          <Person
+            :image="person.image"
+            :name="person.name"
+            :role="person.role"
+            :organisation="person.organisation"
+          />
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -12,7 +24,20 @@
 <script>
   export default {
     props: {
-      id: { type: String, required: true },
+      id: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      persons: {
+        type: Array,
+        required: true,
+      },
     },
   }
 </script>
+
+<style src="./PeopleSection.scss" lang="scss"></style>
