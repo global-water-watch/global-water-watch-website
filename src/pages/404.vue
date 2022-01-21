2c6a3cda27c5ce1c99011c4062815c30e7bc678c
Vincent van Dijck <vincent@voorhoede.nl>
2022-01-21 15:55:28 +0100

    feat(pages) 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,14 @@
 <template>
-  <Fragment />
+  <section class="error-404 layout-section layout-section--padded layout-section--lined">
+    <div class="layout-container">
+      <h1 class="h1">
+        404 - Not Found
+      </h1>
+      <NuxtLink to="/" class="v-btn v-btn--has-bg theme--light elevation-0 v-size--large primary">
+        Home page
+      </NuxtLink>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -7,3 +16,13 @@
     layout: 'error',
   }
 </script>
+
+<style lang="scss">
+  .error-404 .layout-container {
+    text-align: center;
+  }
+
+  .error-404 .v-btn {
+    margin-top: $space-default;
+  }
+</style>
