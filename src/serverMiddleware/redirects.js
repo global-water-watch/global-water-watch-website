e4ee973c1fa16ae68c076db12f1cac48b8d274b0
Vincent van Dijck <vincent@voorhoede.nl>
2022-01-21 16:20:07 +0100

    fix(redirects) extend logic

--- src/serverMiddleware/redirects.js
+++ src/serverMiddleware/redirects.js
@@ -1,19 +1,34 @@
-export default function (req, res, next) {
-  const redirects = [
-    {
-      from: '/blog',
-      to: '/blog/the-quick-brown-fox-jumps',
-    },
-    {
-      from: '/blog/',
-      to: '/blog/the-quick-brown-fox-jumps',
-    },
-  ]
+const redirects = [
+  {
+    from: '/blog',
+    to: '/blog/the-quick-brown-fox-jumps',
+  },
+  {
+    from: '/blog/',
+    to: '/blog/the-quick-brown-fox-jumps',
+  },
+]
 
-  const redirect = redirects.find(r => r.from === req.url)
+export default function (req, res, next) {
+  const url = req.url.split('?')[0]
+  let urlParams = null
+
+  if (req.url.includes('?')) {
+    urlParams = '?' + req.url.split('?')[1]
+  }
+
+  const redirect = redirects.find(r => r.from === url)
 
   if (redirect) {
-    res.writeHead(301, { Location: redirect.to })
+    let newLocation
+
+    if (urlParams) {
+      newLocation = redirect.to + urlParams
+    } else {
+      newLocation = redirect.to
+    }
+
+    res.writeHead(301, { Location: newLocation })
     res.end()
   } else {
     next()
