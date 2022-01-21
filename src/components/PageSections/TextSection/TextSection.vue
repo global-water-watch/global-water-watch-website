f610b76a15187805f062394b34b647318c8bf53f
Vincent van Dijck <vincent@voorhoede.nl>
2022-01-21 13:18:02 +0100

    fix(text-section) put classes on component

--- src/components/PageSections/TextSection/TextSection.vue
+++ src/components/PageSections/TextSection/TextSection.vue
@@ -1,12 +1,10 @@
 <template>
   <section :id="id" class="layout-section layout-section--padded layout-section--lined text-section">
-    <div class="layout-container layout-container--medium">
-      <StructuredText
-        class="text-section__text"
-        :text="text"
-        :links="text.links"
-      />
-    </div>
+    <StructuredText
+      class="layout-container layout-container--medium"
+      :text="text"
+      :links="text.links"
+    />
   </section>
 </template>
 
