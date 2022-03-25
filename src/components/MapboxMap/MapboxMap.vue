d987f6d031d066fb31c52fa823873fa7a531cfe9
Vincent van Dijck <vincent@voorhoede.nl>
2022-03-25 16:16:15 +0100

    feat(components) basic MapboxMap component

---
+++ src/components/MapboxMap/MapboxMap.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="mapbox-map">
+    <v-mapbox
+      ref="vmapbox"
+      class="mapbox-map__map"
+      :access-token="accessToken"
+      :center="center"
+      :map-style="mapboxStyle"
+      :zoom="zoom"
+    >
+      <slot />
+    </v-mapbox>
+  </div>
+</template>
+
+<script>
+  const MAP_ZOOM = 7
+  const NETHERLANDS_CENTER_LATITUDE = 52.1326
+  const NETHERLANDS_CENTER_LONGITUDE = 5.2913
+  const MAP_CENTER = [NETHERLANDS_CENTER_LONGITUDE, NETHERLANDS_CENTER_LATITUDE]
+
+  export default {
+    props: {
+      accessToken: {
+        type: String,
+        required: true,
+      },
+      center: {
+        type: Array,
+        default: MAP_CENTER,
+      },
+      mapboxStyle: {
+        type: String,
+        required: true,
+      },
+      zoom: {
+        type: Number,
+        default: MAP_ZOOM,
+      },
+    },
+    mounted () {
+      const { map } = this.$refs.vmapbox
+
+      // Map events: https://docs.mapbox.com/mapbox-gl-js/api/map/#map-events
+      map.on('resize', event => this.$emit('resize', event))
+      map.on('remove', event => this.$emit('remove', event))
+
+      // Lifecycle events: https://docs.mapbox.com/mapbox-gl-js/api/map/#events-lifecycle
+      map.on('load', event => this.$emit('load', event))
+      map.on('render', event => this.$emit('render', event))
+      map.on('idle', event => this.$emit('idle', event))
+      map.on('error', event => this.$emit('error', event))
+
+      // Data loading events: https://docs.mapbox.com/mapbox-gl-js/api/map/#events-data-loading
+      map.on('data', event => this.$emit('data', event))
+      map.on('styledata', event => this.$emit('styledata', event))
+      map.on('sourcedata', event => this.$emit('sourcedata', event))
+    },
+  }
+</script>
+
+<style src="./MapboxMap.scss" lang="scss"></style>
