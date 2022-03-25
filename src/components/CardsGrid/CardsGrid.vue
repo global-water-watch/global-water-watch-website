728e00597b2afff68101f7c42ebe116a3db29cf6
Vincent van Dijck <vincent@voorhoede.nl>
2022-03-25 11:47:51 +0100

    fix(cards-grid) get rid of extra div

--- src/components/CardsGrid/CardsGrid.vue
+++ src/components/CardsGrid/CardsGrid.vue
@@ -1,20 +1,18 @@
 <template>
   <section class="layout-section layout-section--padded layout-section--lined cards-grid">
-    <div class="layout-container">
-      <ul v-if="items.length" class="cards-grid__grid">
-        <li v-for="item in items" :key="item.id">
-          <CardItem
-            :author="item.heroes[0].author"
-            :created-at="item.createdAt"
-            :image="item.heroes[0].image"
-            :slug="item.slug"
-            :model="item._modelApiKey"
-            :text="item.heroes[0].text"
-            :title="item.title"
-          />
-        </li>
-      </ul>
-    </div>
+    <ul v-if="items.length" class="layout-container cards-grid__grid">
+      <li v-for="item in items" :key="item.id">
+        <CardItem
+          :author="item.heroes[0].author"
+          :created-at="item.createdAt"
+          :image="item.heroes[0].image"
+          :slug="item.slug"
+          :model="item._modelApiKey"
+          :text="item.heroes[0].text"
+          :title="item.title"
+        />
+      </li>
+    </ul>
   </section>
 </template>
 
