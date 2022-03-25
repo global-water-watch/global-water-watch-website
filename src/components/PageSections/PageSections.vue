72dd7677ed8d60c3509c2da1a463466785ace25c
Vincent van Dijck <vincent@voorhoede.nl>
2022-03-25 13:17:21 +0100

    feat(page-sections) new QuoteSection (#85)

--- src/components/PageSections/PageSections.vue
+++ src/components/PageSections/PageSections.vue
@@ -15,17 +15,19 @@
   import MultiTextSection from '@/components/PageSections/MultiTextSection/MultiTextSection'
   import NewsletterSection from '@/components/PageSections/NewsletterSection/NewsletterSection'
   import PeopleSection from '@/components/PageSections/PeopleSection/PeopleSection'
+  import QuoteSection from '@/components/PageSections/QuoteSection/QuoteSection'
   import TabsSection from '@/components/PageSections/TabsSection/TabsSection'
   import TextSection from '@/components/PageSections/TextSection/TextSection'
   import TimelineSection from '@/components/PageSections/TimelineSection/TimelineSection'
 
   const componentsByType = {
+    blog_quote: QuoteSection,
+    blog_text_section: TextSection,
     logo_section: LogoSection,
     multi_text_section: MultiTextSection,
     newsletter_section: NewsletterSection,
     people_section: PeopleSection,
     tabs_section: TabsSection,
-    blog_text_section: TextSection,
     timeline_section: TimelineSection,
   }
   const supportedTypes = Object.keys(componentsByType)
