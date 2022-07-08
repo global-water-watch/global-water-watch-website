078375db166393d413f6d7d2fe9f977d7ceaa546
Bob Bol <bob@voorhoede.nl>
2022-07-08 15:31:46 +0200

    working shared component to fetch data for multi-reservoir pages

---
+++ src/lib/shared-pages/MultiReservoirPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <Fragment>
+    <client-only>
+      <map-to-query-geometry
+        v-if="id && layer && areaType"
+        :id="id"
+        :area-type="areaType"
+        :layer="layer"
+        @found-geometry="onGeometry"
+      />
+      <h1>REUSABLE MULTI REGION PAGE</h1>
+      <pre>{{ reservoirs }}</pre>
+    </client-only>
+  </Fragment>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      layer: null,
+      id: null,
+      areaType: null,
+      reservoirs: [],
+    }),
+
+    mounted () {
+      const { slug } = this.$route.params
+      if (!slug) { return }
+      const [layer, id] = slug.split('--')
+      this.layer = layer
+      this.id = id
+      this.areaType = this.$route.path
+        .split('/')
+        .filter(Boolean)[0]
+    },
+
+    methods: {
+      onGeometry (geometry) {
+        this.$repo.reservoir.getByGeometry(geometry)
+          .then((reservoirs) => {
+            this.reservoirs = reservoirs
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+      },
+    },
+  }
+</script>
