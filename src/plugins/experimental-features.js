4dec73bfaa7eda8c666fef86ba67959595e79463
Zowie van Geest <zowie@zowievangeest.nl>
2022-07-12 10:17:22 +0200

    feat: replace shortcut with konamiCodes

--- src/plugins/experimental-features.js
+++ src/plugins/experimental-features.js
@@ -1,9 +1,33 @@
 export default ({ store }) => {
+  const konamiCodes = [
+    'arrowup',
+    'arrowup',
+    'arrowdown',
+    'arrowdown',
+    'arrowleft',
+    'arrowright',
+    'arrowleft',
+    'arrowright',
+    'b',
+    'a',
+  ]
+
+  let index = 0
+
   document.onkeydown = function (e) {
-    if (e.key === 'p' && (e.ctrlKey || e.metaKey) && e.shiftKey) {
-      e.preventDefault()
-      const experimentalFeatureEnabled = store.getters['ui/showExperimentalFeatures']
-      store.commit('ui/SET_SHOW_EXPERIMENTAL_FEATURES', !experimentalFeatureEnabled)
+    e.preventDefault()
+
+    const key = e.key.toLowerCase()
+
+    if (key === konamiCodes[index]) {
+      index++
+      if (index === konamiCodes.length) {
+        const experimentalFeatureEnabled = store.getters['ui/showExperimentalFeatures']
+        store.commit('ui/SET_SHOW_EXPERIMENTAL_FEATURES', !experimentalFeatureEnabled)
+        index = 0
+      }
+    } else {
+      index = 0
     }
   }
 }
