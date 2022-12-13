80ab51485f3f414622782720e8ea6182397aa83e
Lucia Sarni <lucia@voorhoede.nl>
2022-12-13 15:48:02 +0100

    feat: Add buttons to load next/prev satellite image on the comparison map

--- src/components/ComparisonMap/ComparisonMap.vue
+++ src/components/ComparisonMap/ComparisonMap.vue
@@ -7,6 +7,7 @@
         :date="oldDate"
         class="comparison-map__detail-map"
         @setMap="onSetOldMap"
+        @loading="onLoadingSatelliteImage"
       />
 
       <ComparisonDetailMap
@@ -15,12 +16,57 @@
         :date="date"
         class="comparison-map__detail-map"
         @setMap="onSetCurrentMap"
+        @loading="onLoadingSatelliteImage"
       />
     </div>
 
     <div class="comparison-map__dates">
-      <span>{{ parseDate(oldDate) }}</span>
-      <span>{{ parseDate(date) }}</span>
+      <div>
+        <v-btn
+          icon
+          color="primary"
+          :disabled="!previousOldDate || isLoadingSatelliteImages"
+          :title="parseDate(previousOldDate)"
+          @click="() => oldDateIndex = previousOldDateIndex"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <span>{{ parseDate(oldDate) }}</span>
+        <v-btn
+          icon
+          color="primary"
+          :disabled="!nextOldDate || isLoadingSatelliteImages"
+          :title="parseDate(nextOldDate)"
+          @click="() => oldDateIndex = nextOldDateIndex"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </div>
+      <span v-if="isLoadingSatelliteImages">
+        Loading satellite images
+        <v-progress-circular indeterminate :size="18" :width="2" class="comparison-map__loading" />
+      </span>
+      <div>
+        <v-btn
+          icon
+          color="primary"
+          :disabled="!previousDate || isLoadingSatelliteImages"
+          :title="parseDate(previousDate)"
+          @click="() => dateIndex = previousDateIndex"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <span>{{ parseDate(date) }}</span>
+        <v-btn
+          icon
+          color="primary"
+          :disabled="!nextDate || isLoadingSatelliteImages"
+          :title="parseDate(nextDate)"
+          @click="() => dateIndex = nextDateIndex"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </div>
     </div>
   </div>
 </template>
@@ -34,34 +80,70 @@
         type: Array,
         required: true,
       },
+      timeSeries: {
+        type: Array,
+        default: () => [],
+      },
     },
 
     data () {
       return {
-        oldSatelliteImageUrl: '',
-        satelliteImageUrl: '',
+        dateIndex: -1,
+        oldDateIndex: -1,
+        isLoadingSatelliteImages: false,
       }
     },
 
     computed: {
       date () {
-        return new Date()
+        return this.timeSeriesDates[this.dateIndex]
       },
       oldDate () {
-        const date = new Date()
-        date.setFullYear(date.getFullYear() - 1)
-        return date
+        return this.timeSeriesDates[this.oldDateIndex]
+      },
+      timeSeriesDates () {
+        return this.timeSeries.map(item => new Date(item[0]))
+      },
+      previousDateIndex () {
+        return this.dateIndex - 1
+      },
+      nextDateIndex () {
+        return this.dateIndex + 1
+      },
+      previousDate () {
+        return this.timeSeriesDates[this.previousDateIndex]
+      },
+      nextDate () {
+        return this.timeSeriesDates[this.nextDateIndex]
+      },
+      previousOldDateIndex () {
+        return this.oldDateIndex - 1
+      },
+      nextOldDateIndex () {
+        return this.oldDateIndex + 1
+      },
+      previousOldDate () {
+        return this.timeSeriesDates[this.previousOldDateIndex]
+      },
+      nextOldDate () {
+        return this.timeSeriesDates[this.nextOldDateIndex]
       },
     },
 
     async mounted () {
+      const date = this.timeSeriesDates[this.timeSeriesDates.length - 1]
+      const oldDate = new Date(date.getTime())
+      oldDate.setFullYear(date.getFullYear() - 1)
+      this.dateIndex = this.timeSeriesDates.length - 1
+      this.oldDateIndex = this.getNearestDateIndex(oldDate)
+
       await this.initializeMap()
     },
 
     methods: {
       parseDate (date) {
         const options = { year: 'numeric', month: 'long', day: 'numeric' }
-        return date.toLocaleDateString('en-EN', options)
+        return date?.toLocaleDateString('en-EN', options)
       },
       onSetCurrentMap (map) {
         currentMap = map
@@ -74,6 +156,25 @@
         const MapboxCompare = (await import('mapbox-gl-compare')).default
         // eslint-disable-next-line no-new
         new MapboxCompare(oldMap, currentMap, '#comparison-map-container')
+      },
+      onLoadingSatelliteImage (isLoading) {
+        this.isLoadingSatelliteImages = isLoading
+      },
+      getNearestDateIndex (target) {
+        let bestDate = this.timeSeriesDates.length
+
+        let bestDiff = Infinity
+        let currDiff = 0
+
+        this.timeSeriesDates.forEach((date, index) => {
+          currDiff = Math.abs(target - date)
+          if (currDiff < bestDiff) {
+            bestDate = index
+            bestDiff = currDiff
+          }
+        })
+
+        return bestDate
       },
     },
   }
