68c6a3e51bc5829843408df28e0e9dc8f44f12cd
Lucia Sarni <lucia@voorhoede.nl>
2023-03-10 12:37:25 +0100

    fix: include description for reservoirs

--- src/lib/layers/reservoirs.js
+++ src/lib/layers/reservoirs.js
@@ -6,6 +6,9 @@ export default {
   id: 'reservoirsv10',
   checked: true,
   promoteId: 'fid', // this id is used to identify the hover id in the map.
+  description: `You can select multiple reservoirs by keeping Ctrl / Cmd (⌘) pressed when clicking on the reservoirs.
+                To deselect a reservoir click on it again.
+                To see the details for the selected reservoirs click on one of them without pressing Ctrl / Cmd (⌘).`,
   source: {
     type: 'vector',
     url: 'mapbox://global-water-watch.reservoirs-v10',
