7057377203328b124e59573c9b17c145b074ec26
Lucia Sarni <lucia@voorhoede.nl>
2023-06-12 11:44:12 +0200

    Display a surface volume area for single reservoirs

--- src/repo/reservoirRepo.js
+++ src/repo/reservoirRepo.js
@@ -1,42 +1,36 @@
 import qs from 'qs'
 import { capitalize } from '@/lib/primitive-helpers'
 
-export default function (axios) {
-  return {
-    // Get reservoir by id (fid)
-    getById: id => axios.$get(`reservoir/${id}`),
-
-    // Get only the service water area (raw data)
-    getTimeSeriesById: id =>
-      axios
-        .$get(`reservoir/${id}/ts/surface_water_area`)
-        .then(timeSeries => formatTimeSeries(id, timeSeries)),
-
-    getByGeometry: geometry =>
-      axios.post('reservoir/geometry', geometry).then(({ data }) => data),
-
-    getTimeSeriesByGeometry: (geometry, variableName, period) =>
-      axios.post(`reservoir/geometry/ts/${variableName}?agg_period=${period}`, geometry).then(formatMultipleTimeSeries),
-
-    getByIds: ids => axios.$get(`reservoir/?${qs.stringify({ ids, limit: ids.length }, { indices: false })}`),
-
-    getTimeSeriesByIds: (ids, variableName, period) => {
-      return axios
-        .$get(`ts/?${qs.stringify({
-          reservoir_ids: ids,
-          variable_name: variableName,
-          agg_period: period,
-        }, { indices: false })}`)
-        .then(formatMultipleTimeSeries)
-    },
+const valueConversion = ({ t, value, unit }) => {
+  if (unit === 'm2') {
+    // Convert to km2
+    return [t, (value / 1000000).toFixed(2)]
+  } else if (unit === 'm3') {
+    // Convert to km3
+    return [t, (value / 1000000000).toFixed(2)]
+  } else {
+    return [t, value]
   }
 }
 
-// TODO: make sure this km2 comes from the backend again as an unit
-// const valueUnit = timeSeries[0]?.unit
-const valueUnit = 'km2'
+const unitConversion = (unit) => {
+  if (unit === 'm2') {
+    return 'km2'
+  } else if (unit === 'm3') {
+    return 'km3'
+  } else {
+    return unit
+  }
+}
 
-const timeSeriesAxis = (yAxisName) => {
+const formatTimeSeries = (id, timeSeries) => {
+  if (timeSeries.length === 0) { return null }
+  const valueName = `${timeSeries[0]?.name?.split('_').map(capitalize).join(' ')} (#${id})`
+
+  const valueUnit = unitConversion(timeSeries[0].unit)
+
+  const data = timeSeries.map(valueConversion)
+
   return {
     xAxis: [
       {
@@ -53,27 +47,10 @@ const timeSeriesAxis = (yAxisName) => {
     ],
     yAxis: [
       {
-        name: `${yAxisName} (${valueUnit})`,
+        name: `${valueName} (${valueUnit})`,
         type: 'value',
       },
     ],
-  }
-}
-
-const parseTimeSeriesData = (data) => {
-  return (data || []).map(({ t, value: valueInM2 }) => {
-    const value = (valueInM2 / 1000000).toFixed(2)
-    return [t, value]
-  })
-}
-
-const formatTimeSeries = (id, timeSeries) => {
-  const valueName = `${timeSeries[0]?.name?.split('_').map(capitalize).join(' ')} (#${id})`
-
-  const data = parseTimeSeriesData(timeSeries)
-
-  return {
-    ...timeSeriesAxis(valueName),
     series: [
       {
         name: valueName,
@@ -95,12 +72,14 @@ const formatMultipleTimeSeries = (response) => {
     .map(capitalize)
     .join(' ')
 
+  const valueUnit = unitConversion(data?.variable_unit)
+
   if (data?.data?.length > 0) {
     series.push({
       name: 'Sum',
       type: 'line',
       areaStyle: {},
-      data: parseTimeSeriesData(data?.data),
+      data: data?.data?.map(valueConversion),
     })
   }
 
@@ -109,13 +88,59 @@ const formatMultipleTimeSeries = (response) => {
       series.push({
         name: `${valueName} (#${key})`,
         type: 'line',
-        data: parseTimeSeriesData(data.source_data[key]),
+        data: data.source_data[key].map(valueConversion),
       })
     })
   }
 
   return {
-    ...timeSeriesAxis(valueName),
+    xAxis: [{
+      type: 'time',
+      axisPointer: {
+        label: {
+          show: true,
+        },
+        handle: {
+          show: true,
+        },
+      },
+    }],
+    yAxis: [
+      {
+        name: `${valueName} (${valueUnit})`,
+        type: 'value',
+      },
+    ],
     series,
+  }
+}
+
+export default function (axios) {
+  return {
+    // Get reservoir by id (fid)
+    getById: id => axios.$get(`reservoir/${id}`),
+
+    getTimeSeriesById: (id, variableName) =>
+      axios
+        .$get(`reservoir/${id}/ts/${variableName}`)
+        .then(timeSeries => formatTimeSeries(id, timeSeries)),
+
+    getByGeometry: geometry =>
+      axios.post('reservoir/geometry', geometry).then(({ data }) => data),
+
+    getTimeSeriesByGeometry: (geometry, variableName, period) =>
+      axios.post(`reservoir/geometry/ts/${variableName}?agg_period=${period}`, geometry).then(formatMultipleTimeSeries),
+
+    getByIds: ids => axios.$get(`reservoir/?${qs.stringify({ ids, limit: ids.length }, { indices: false })}`),
+
+    getTimeSeriesByIds: (ids, variableName, period) => {
+      return axios
+        .$get(`ts/?${qs.stringify({
+          reservoir_ids: ids,
+          variable_name: variableName,
+          agg_period: period,
+        }, { indices: false })}`)
+        .then(formatMultipleTimeSeries)
+    },
   }
 }
